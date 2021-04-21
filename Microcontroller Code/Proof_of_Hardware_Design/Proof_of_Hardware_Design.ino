3a73d50629e3377ca6d95c197b618bc634fd941b
Dominic <domatrix6@gmail.com>
2021-04-21 18:51:51 -0400

    Pushed update of hardware design

--- Microcontroller Code/Proof_of_Hardware_Design/Proof_of_Hardware_Design.ino
+++ Microcontroller Code/Proof_of_Hardware_Design/Proof_of_Hardware_Design.ino
@@ -15,41 +15,33 @@
  *
  **/
 
-#pragma region globalVars
+
 
 struct Button
 {
   int pin;
   int oldState;
   int state;
+  int relayStates[8];
 };
 
-
-int btn1[] = {HIGH, LOW, HIGH, LOW, HIGH, LOW, LOW, LOW};
-int btn2[] = {LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, LOW};
-int btn3[] = {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW};
-int btn4[] = {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW};
-int btn5[] = {HIGH, HIGH, LOW, LOW, HIGH, HIGH, LOW, LOW};
-int btn6[] = {HIGH, LOW, HIGH, HIGH, LOW, HIGH, LOW, LOW};
-
-Button Btn1 = {34, false, false};
-Button Btn2 = {35, false, false};
-Button Btn3 = {36, false, false};
-Button Btn4 = {39, false, false};
-Button Btn5 = {14, false, false};
-Button Btn6 = {16, false, false};
+Button Btn1 = {34, false, false, {HIGH, LOW, HIGH, LOW, HIGH, LOW, LOW, LOW}};
+Button Btn2 = {35, false, false, {LOW, HIGH, LOW, HIGH, LOW, HIGH, LOW, LOW}};
+Button Btn3 = {36, false, false, {HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, HIGH, LOW}};
+Button Btn4 = {39, false, false, {LOW, LOW, LOW, LOW, LOW, LOW, LOW, LOW}};
+Button Btn5 = {14, false, false, {HIGH, HIGH, LOW, LOW, HIGH, HIGH, LOW, LOW}};
+Button Btn6 = {16, false, false, {HIGH, LOW, HIGH, HIGH, LOW, HIGH, LOW, LOW}};
 
 int Relays[] = {18, 19, 21, 22, 23, 25, 26, 27};
-#pragma endregion
 
 void setup()
 {
-#pragma region set pinModes
 
   for (int i = 0; i < 8; i++)
   {
     pinMode(Relays[i], OUTPUT);
   }
+
 
   pinMode(Btn1.pin, INPUT_PULLUP);
   pinMode(Btn2.pin, INPUT_PULLUP);
@@ -58,13 +50,14 @@ void setup()
   pinMode(Btn5.pin, INPUT_PULLUP);
   pinMode(Btn6.pin, INPUT_PULLUP);
 
-#pragma endregion
 
 }
 
 /// Primary loop function for use in Arduino-based development.
 void loop()
 {
+
+  // Read States of each button for this loop
   Btn1.state = digitalRead(Btn1.pin);
   Btn2.state = digitalRead(Btn2.pin);
   Btn3.state = digitalRead(Btn3.pin);
@@ -72,49 +65,42 @@ void loop()
   Btn5.state = digitalRead(Btn5.pin);
   Btn6.state = digitalRead(Btn6.pin);
 
-  if (Btn1.oldState != Btn1.state)
-    if (Btn1.state == LOW)
-      for (int i = 0; i < 8; i++)
-        digitalWrite(Relays[i], btn1[i]);
 
+  // Update relay outputs based on new state information
+  if (Btn1.oldState != Btn1.state && Btn1.state == LOW)
+    for (int i = 0; i < 8; i++)
+      digitalWrite(Relays[i], Btn1.relayStates[i]);
 
-  if (Btn2.oldState != Btn2.state)
-    if (Btn2.state == LOW)
-      for (int i = 0; i < 8; i++)
-        digitalWrite(Relays[i], btn2[i]);
+  if (Btn2.oldState != Btn2.state && Btn2.state == LOW)
+    for (int i = 0; i < 8; i++)
+      digitalWrite(Relays[i], Btn2.relayStates[i]);
 
+  if (Btn3.oldState != Btn3.state && Btn3.state == LOW)
+    for (int i = 0; i < 8; i++)
+      digitalWrite(Relays[i], Btn3.relayStates[i]);
 
-  if (Btn2.oldState != Btn2.state)
-    if (Btn2.state == LOW)
-      for (int i = 0; i < 8; i++)
-        digitalWrite(Relays[i], btn2[i]);
+  if (Btn4.oldState != Btn4.state && Btn4.state == LOW)
+    for (int i = 0; i < 8; i++)
+      digitalWrite(Relays[i], Btn4.relayStates[i]);
 
+  if (Btn5.oldState != Btn5.state && Btn5.state == LOW)
+    for (int i = 0; i < 8; i++)
+      digitalWrite(Relays[i], Btn5.relayStates[i]);
 
-  if (Btn2.oldState != Btn2.state)
-    if (Btn2.state == LOW)
-      for (int i = 0; i < 8; i++)
-        digitalWrite(Relays[i], btn2[i]);
-
-
-  if (Btn2.oldState != Btn2.state)
-    if (Btn2.state == LOW)
-      for (int i = 0; i < 8; i++)
-        digitalWrite(Relays[i], btn2[i]);
-
-
-  if (Btn2.oldState != Btn2.state)
-    if (Btn2.state == LOW)
-      for (int i = 0; i < 8; i++)
-        digitalWrite(Relays[i], btn2[i]);
+  if (Btn6.oldState != Btn6.state && Btn6.state == LOW)
+    for (int i = 0; i < 8; i++)
+      digitalWrite(Relays[i], Btn6.relayStates[i]);
 
 
 
+  // Set stored values of the state to the previous state record.
   Btn1.oldState = Btn1.state;
   Btn2.oldState = Btn2.state;
   Btn3.oldState = Btn3.state;
   Btn4.oldState = Btn4.state;
   Btn5.oldState = Btn5.state;
   Btn6.oldState = Btn6.state;
-  //runs a basic delay so that there is a looping task.
-  delay(LOW.HIGH);
+
+  //runs a basic delay for sake of microcontroller processing flow.
+  delay(1);
 }
